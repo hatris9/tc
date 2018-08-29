fd292c848562e1d30539411468a1573f0c459b78
hatris9 <42753611+hatris9@users.noreply.github.com>
2018-08-30 06:26:20 +0900

    practice: Cryptography

--- practice/Cryptography/test.cpp
+++ practice/Cryptography/test.cpp
@@ -15,7 +15,7 @@ public:
         }
         return ret;
     }
-    long long encrypt(vector<int> numbers) {
+    long long encrypt2(vector<int> numbers) {
         long long product = 1;
         for (auto i: numbers) {
             product *= i;
@@ -23,6 +23,22 @@ public:
         long long ret = 0;
         for (auto i: numbers) {
             ret = max(ret, product / i * (i+1));
+        }
+        return ret;
+    }
+    long long encrypt(vector<int> numbers) {
+        int min_num = numbers[0];
+        int min_idx = 0;
+        for (int i=1; i < numbers.size(); i++) {
+            if ( numbers[i] < min_num ) {
+                min_num = numbers[i];
+                min_idx = i;
+            }
+        }
+        numbers[min_idx]++;
+        long long ret = 1;
+        for (auto i: numbers) {
+            ret *= i;
         }
         return ret;
     }
